426dbd8f6ca17126a80cb9337dd61feba7682d22
benjamin.slabbert <benjamin.slabbert@gmail.com>
2019-09-05 15:29:01 +0200

    initial commit

---
+++ src/main.cpp
@@ -0,0 +1,7 @@
+#include "spdlog/sinks/stdout_color_sinks.h" // or "../stdout_sinks.h" if no colors needed
+#include "spdlog/spdlog.h"
+
+int main() {
+    SPDLOG_INFO("Hello from spdlog!");
+    return 0;
+}
